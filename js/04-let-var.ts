0b90cdad00f9b4a449ebcfa216dff1f2dd5d7a4f
Jonathan IMG <join_mg@hotmail.com>
2018-09-06 19:15:51 -0500

    Let and Var
    JS from TS

---
+++ js/04-let-var.ts
@@ -0,0 +1,15 @@
+/*Let and var
+var = global
+let = specifci scope */
+
+var numero1 = 10;
+var numero2 = 12;
+
+if(numero1 == 10){
+    let numero1 = 44;
+    var numero2 = 55;
+
+    console.log(numero1, numero2);
+}
+
+console.log(numero1, numero2);
